031c1817e2754b2fb94104d0fd28c254b5c745b9
kmw10693 <kmw10693@konkuk.ac.kr>
2024-05-02 02:28:14 +0900

    feat : initialize jdbc structure and test code

---
+++ src/test/java/next/dao/UserDaoTest.java
@@ -0,0 +1,45 @@
+package next.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import core.dao.UserDao;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import core.jdbc.ConnectionManager;
+import core.model.User;
+
+public class UserDaoTest {
+    @Before
+    public void setup() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("jwp.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    public void crud() throws Exception {
+        User expected = new User("userId", "password", "김민우", "minunu12@email.com");
+        UserDao userDao = new UserDao();
+        userDao.insert(expected);
+        User actual = userDao.findByUserId(expected.getUserId());
+        assertEquals(expected, actual);
+
+        expected.update(new User("userId", "password2", "고양이", "myamya@email.com"));
+        userDao.update(expected);
+        actual = userDao.findByUserId(expected.getUserId());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        UserDao userDao = new UserDao();
+        List<User> users = userDao.findAll();
+        assertEquals(1, users.size());
+    }
+}
