f33af83d4d6e023cbeafc0e05b8721edb27e98bd
JangIkhwan <87964632+JangIkhwan@users.noreply.github.com>
2024-05-09 11:25:47 +0900

    feat : update, findAll 메소드 구현

--- src/main/java/core/dao/UserDao.java
+++ src/main/java/core/dao/UserDao.java
@@ -37,11 +37,55 @@ public class UserDao {
 
     public void update(User user) throws SQLException {
         // TODO 구현 필요함.
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        con = ConnectionManager.getConnection();
+        String sql = "UPDATE users " +
+                "SET password=?, name = ?, email=? " +
+                "WHERE userId = ?";
+        pstmt = con.prepareStatement(sql);
+        pstmt.setString(1, user.getPassword());
+        pstmt.setString(2, user.getName());
+        pstmt.setString(3, user.getEmail());
+        pstmt.setString(4, user.getUserId());
+        pstmt.executeUpdate();
+
+        if (pstmt != null) {
+            pstmt.close();
+        }
+
+        if (con != null) {
+            con.close();
+        }
     }
 
     public List<User> findAll() throws SQLException {
         // TODO 구현 필요함.
-        return new ArrayList<User>();
+        Connection con = null;
+
+        con = ConnectionManager.getConnection();
+        String sql = "SELECT userId, password, name, email FROM users";
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery();
+
+        ArrayList<User> users = new ArrayList<>();
+        while(rs.next()){
+            users.add(new User(rs.getString("userId"), rs.getString("password"),
+                    rs.getString("name"), rs.getString("email")));
+        }
+
+        if(con != null){
+            con.close();
+        }
+        if(pstmt != null){
+            pstmt.close();
+        }
+        if(rs != null){
+            rs.close();
+        }
+
+        return users;
     }
 
     public User findByUserId(String userId) throws SQLException {
