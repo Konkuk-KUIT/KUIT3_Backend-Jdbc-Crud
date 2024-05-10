220fee5691a7062de083267134158f05cd804e13
JangIkhwan <87964632+JangIkhwan@users.noreply.github.com>
2024-05-10 09:49:40 +0900

    Fix : try-finally 적용

--- src/main/java/core/dao/UserDao.java
+++ src/main/java/core/dao/UserDao.java
@@ -39,53 +39,60 @@ public class UserDao {
         // TODO 구현 필요함.
         Connection con = null;
         PreparedStatement pstmt = null;
-
-        con = ConnectionManager.getConnection();
-        String sql = "UPDATE users " +
-                "SET password=?, name = ?, email=? " +
-                "WHERE userId = ?";
-        pstmt = con.prepareStatement(sql);
-        pstmt.setString(1, user.getPassword());
-        pstmt.setString(2, user.getName());
-        pstmt.setString(3, user.getEmail());
-        pstmt.setString(4, user.getUserId());
-        pstmt.executeUpdate();
-
-        if (pstmt != null) {
-            pstmt.close();
+        try{
+            con = ConnectionManager.getConnection();
+            String sql = "UPDATE users " +
+                    "SET password=?, name = ?, email=? " +
+                    "WHERE userId = ?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+            pstmt.executeUpdate();
         }
+        finally{
+            if (pstmt != null) {
+                pstmt.close();
+            }
 
-        if (con != null) {
-            con.close();
+            if (con != null) {
+                con.close();
+            }
         }
     }
 
     public List<User> findAll() throws SQLException {
         // TODO 구현 필요함.
         Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
 
-        con = ConnectionManager.getConnection();
-        String sql = "SELECT userId, password, name, email FROM users";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        ResultSet rs = pstmt.executeQuery();
+        try{
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT userId, password, name, email FROM users";
+            pstmt = con.prepareStatement(sql);
+            rs = pstmt.executeQuery();
 
-        ArrayList<User> users = new ArrayList<>();
-        while(rs.next()){
-            users.add(new User(rs.getString("userId"), rs.getString("password"),
-                    rs.getString("name"), rs.getString("email")));
+            ArrayList<User> users = new ArrayList<>();
+            while(rs.next()){
+                users.add(new User(rs.getString("userId"), rs.getString("password"),
+                        rs.getString("name"), rs.getString("email")));
+            }
+            return users;
         }
+        finally {
+            if(con != null){
+                con.close();
+            }
+            if(pstmt != null){
+                pstmt.close();
+            }
+            if(rs != null){
+                rs.close();
+            }
 
-        if(con != null){
-            con.close();
         }
-        if(pstmt != null){
-            pstmt.close();
-        }
-        if(rs != null){
-            rs.close();
-        }
-
-        return users;
     }
 
     public User findByUserId(String userId) throws SQLException {
